fd2c8e984ab84e63ce8b93ed626882e72a1175f3
haris <harisiftikhar109@gmail.com>
2022-07-01 20:36:18 +0500

    CRUD operations

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('posts/', views.posts, name="posts"),
     path('post/<str:pk>/', views.post, name="post"),
     path('profile/', views.profile, name="profile"),
+
+    # CRUD PATHS
+    path('create_post/', views.createPost, name="create_post"),
+    path('update_post/<str:pk>/', views.updatePost, name="update_post"),
+    path('delete_post/<str:pk>/', views.deletePost, name="delete_post"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -22,3 +24,46 @@ def post(request, pk):
 
 def profile(request):
     return render(request, 'base/profile.html')
+
+
+# CRUD Views
+
+@login_required(login_url="home")
+def createPost(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+@login_required(login_url="home")
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url="home")
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts')
+
+
+    context = {'item':post}
+    return render(request, 'base/delete.html', context)
